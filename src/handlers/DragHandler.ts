41de1df2d633a4fd2311c4ea3fbb00b8722bf45e
Nemika <mmnemijus@gmail.com>
2022-09-02 14:53:15 +0300

    Help

--- src/handlers/DragHandler.ts
+++ src/handlers/DragHandler.ts
@@ -3,17 +3,27 @@ import type { InteractionData, InteractionEvent, Sprite } from "pixi.js";
 export default function DragHandler(sprite: Sprite) {
     let data: null | InteractionData = null
     let dragging: boolean = false
+    let originalX: number = 0
+    let originalY: number = 0
 
     function Start(event: InteractionEvent) {
         data = event.data
         dragging = true
         sprite.alpha = .5
+        originalX = sprite.x
+        originalY = sprite.y
     }
 
     function End() {
         dragging = false
         sprite.alpha = 1
         data = null
+
+        var snapX = originalX
+        var snapY = originalY
+
+        sprite.position.set(snapX, snapY)
+
     }
 
     function Move() {
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PixiInit from "./Bixi"
 
 createApp(App).mount('#app')
+
+document.onload = () => {
+    console.log("Hi")
+    PixiInit()
+}
