44dd2d645f8b80fbf6f7c35a26abcda3a3cfda2b
Nemika__ <mmnemijus@gmail.com>
2022-09-04 04:09:44 +0300

    Remove console logs :3

--- src/lib/handlers/DragHandler.ts
+++ src/lib/handlers/DragHandler.ts
@@ -1,35 +1,33 @@
 import type { InteractionData, InteractionEvent, IPointData, Sprite } from "pixi.js";
 
 export default function DragHandler(sprite: Sprite) {
-    let data: null | InteractionData = null
-    let dragging: boolean = false
+    let data: null | InteractionData = null;
+    let dragging: boolean = false;
 
     function Start(event: InteractionEvent) {
-        data = event.data
-        dragging = true
-        sprite.alpha = .5
+        data = event.data;
+        dragging = true;
+        sprite.alpha = .5;
     }
 
     function End() {
-        dragging = false
-        sprite.alpha = 1
-        data = null
+        dragging = false;
+        sprite.alpha = 1;
+        data = null;
 
-        const snapX = Math.floor(sprite.x / 100)
-        const snapY = Math.floor(sprite.y / 100)
+        const snapX = Math.floor(sprite.x / 100)*100+50;
+        const snapY = Math.floor(sprite.y / 100)*100+50;
 
-        console.log(snapX, snapY)
-
-        sprite.position.set(snapX*100+50, snapY*100+50)
+        sprite.position.set(snapX, snapY);
 
     }
 
     function Move() {
         if (dragging) {
-            const newPos: IPointData | undefined = data?.getLocalPosition(sprite.parent)
+            const newPos: IPointData | undefined = data?.getLocalPosition(sprite.parent);
             if (newPos) {
 
-                sprite.position.set(newPos.x, newPos.y)
+                sprite.position.set(newPos.x, newPos.y);
             }
         }
     }
