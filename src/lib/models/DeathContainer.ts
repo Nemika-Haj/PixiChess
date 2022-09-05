56df46833db6909e672e8f5c4bc4c907e75c7a7b
Nemika <mmnemijus@gmail.com>
2022-09-05 13:13:30 +0300

    Added collected pawn text

--- src/lib/models/DeathContainer.ts
+++ src/lib/models/DeathContainer.ts
@@ -1,4 +1,4 @@
-import { Application, Container, Sprite } from "pixi.js";
+import { Application, Container, Sprite, Text, TextStyle } from "pixi.js";
 import { OBJECT_NAMES } from "../enums";
 import { Pawn } from "./Pawn";
 
@@ -11,27 +11,42 @@ export class DeathContainer extends Container {
 
     constructor(app: Application) {
         super()
-        this.name = OBJECT_NAMES.DEATH_CONTAINER
+        this.name = OBJECT_NAMES.DEATH_CONTAINER;
         this.x = app.view.width - containerDimensions.x - borderMargin;
+
+        const textStyle: TextStyle = new TextStyle({
+            fontFamily: 'Arial',
+            fontWeight: "bold",
+            fill: '#000',
+            stroke: '#000',
+            strokeThickness: .5
+        });
+
+        const blackCollector: Text = new Text("Collected White Pawns", textStyle);
+        blackCollector.position.set(100, 25)
+
+        const whiteCollector: Text = new Text("Collected Black Pawns", textStyle)
+        whiteCollector.position.set(100, 525)
+
+        this.addChild(blackCollector)
+        this.addChild(whiteCollector)
     }
 
     public add(pawn: Pawn, bottom?: boolean) {
-        const newDeadSprite: Sprite = Pawn.fromTexture(pawn.texture)
+        const newDeadSprite: Sprite = Pawn.fromTexture(pawn.texture);
 
-        let x = 100 + (40 * ((!bottom ? this.whitePawns : this.blackPawns) % 7) )
-        let y = (!bottom ? 50 : 400)
+        let x = 100 + (40 * ((!bottom ? this.whitePawns : this.blackPawns) % 7) );
+        let y = (!bottom ? 100 : 600);
         
-        const topMarginMultiplier = Math.floor((!bottom ? this.whitePawns : this.blackPawns) / 7 )
+        const topMarginMultiplier = Math.floor((!bottom ? this.whitePawns : this.blackPawns) / 7 );
 
-        y += 70 * topMarginMultiplier
+        y += 70 * topMarginMultiplier;
 
-        console.log(y)
+        newDeadSprite.position.set(x, y);
 
-        newDeadSprite.position.set(x, y)
+        !bottom ? this.whitePawns++ : this.blackPawns++;
 
-        !bottom ? this.whitePawns++ : this.blackPawns++
-
-        this.addChild(newDeadSprite)
+        this.addChild(newDeadSprite);
     }
 
 }
